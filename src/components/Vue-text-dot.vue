270c634d7610ed9c08f8ccb46e6a0d40f79de619
Julian Scheuchenzuber <js@lvl51.de>
2017-03-23 12:34:14 +0100

    Changed computation method and increased timeout

--- src/components/Vue-text-dot.vue
+++ src/components/Vue-text-dot.vue
@@ -1,60 +1,64 @@
 <template>
-  <div>
-    <div class="text-dot">{{pMsg}}</div>
-  </div>
+    <div>
+        <div class="text-dot">{{pMsg}}</div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'vue-text-dot',
-  props: {
-    line: { type: Number, default: 3 },
-    msg: { type: String, required: true },
-    webfont: { type: Boolean, default: true }
-  },
-  data () {
-    return {
-      pMsg: this.msg,
-      isDot: false
+    export default {
+        name: 'vue-text-dot',
+        props: {
+            line: {type: Number, default: 3},
+            msg: {type: String, required: true},
+            webfont: {type: Boolean, default: true}
+        },
+        data () {
+            return {
+                pMsg: this.msg,
+                isDot: false
+            }
+        },
+        mounted () {
+            
+            // If a webfont was used cut the string once more just to be sure
+            // There is a known problem with the calculation of asnyc loaded font height
+            if (this.webfont)
+                setTimeout(() => {
+                    this.dot();
+                }, 100);
+        },
+        methods: {
+            getHeight: (elem) => {
+                return {
+                    height: Math.min(
+                        elem.clientHeight,
+                        elem.scrollHeight,
+                        elem.offsetHeight),
+                    lineHeight: Math.ceil(parseFloat(window.getComputedStyle(elem).getPropertyValue('line-height')))
+                };
+            },
+            dot () {
+                let dom = this.$el.querySelector('div');
+                let {height, lineHeight} = this.getHeight(dom);
+                
+                while (height > lineHeight * this.line * 3) {
+                    this.isDot = true
+                    this.pMsg = this.pMsg.substring(0, this.pMsg.length / 2)
+                    dom.innerHTML = this.pMsg
+                    height = parseInt(window.getComputedStyle(dom).getPropertyValue('height'));
+                }
+                while (height > lineHeight * this.line) {
+                    this.isDot = true
+                    this.pMsg = this.pMsg.replace(/(\s)*([a-zA-Z0-9]+|\W)(\.\.\.)?$/, "...")
+                    dom.innerHTML = this.pMsg
+                    height = parseInt(window.getComputedStyle(dom).getPropertyValue('height'));
+                }
+                
+                console.log('-');
+                if (this.isDot) {
+                    this.$emit('isDot')
+                }
+            },
+        }
     }
-  },
-  mounted () {
-
-    // If a webfont was used cut the string once more just to be sure
-    // There is a known problem with the calculation of asnyc loaded font height
-    if(this.webfont)
-	    setTimeout(() => {
-	      this.dot();
-	    });
-  },
-  methods: {
-    dot () {
-      let dom = this.$el.querySelector('div')
-      let height = parseInt(window.getComputedStyle(dom)['height'])
-      let lineHeight
-      if (window.getComputedStyle(dom)['line-height'] === 'normal') {
-        console.error("[warn from vue-text-dot] you'd better set css 'line-height' ")
-        lineHeight = parseInt(window.getComputedStyle(dom)['font-size']) * 1.5
-      } else {
-        lineHeight = parseInt(window.getComputedStyle(dom)['line-height'])
-      }
-      while (height > lineHeight * this.line * 3) {
-        this.isDot = true
-        this.pMsg = this.pMsg.substring(0, this.pMsg.length / 2)
-        dom.innerHTML = this.pMsg
-        height = parseInt(window.getComputedStyle(dom)['height'])
-      }
-      while (height > lineHeight * this.line) {
-        this.isDot = true
-        this.pMsg = this.pMsg.replace(/(\s)*([a-zA-Z0-9]+|\W)(\.\.\.)?$/,"...")
-        dom.innerHTML = this.pMsg
-        height = parseInt(window.getComputedStyle(dom)['height'])
-      }
-
-      if (this.isDot) {
-        this.$emit('isDot')
-      }
-    },
-  }
-}
 </script>
